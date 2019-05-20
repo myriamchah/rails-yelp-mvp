191be0d99f15ec27144ab7998d499f8d55089f8d
Myriam Chahlafi <myriam.chah@gmail.com>
2019-05-20 12:12:04 +0200

    Routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:edit, :update, :destroy] do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show]
+
 end
